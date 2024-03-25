a4195ca1320f5f8cfafc29f0306dac28ebb8e995
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-03-25 10:54:35 +0530

    Add files via upload
    Today, we're introducing a program to calculate the area of a square—a small yet significant step forward.

---
+++ P_Qs1.c
@@ -0,0 +1,10 @@
+//Write a program to calculate area of a square.
+#include<stdio.h>
+int main()
+{
+    int side;
+    printf("\nEnter side of a square:");
+    scanf("%d",&side);
+    printf("\nArea: %d",side*side);
+    return 0;
+}
