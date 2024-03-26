843a78b255e90923d7f2244e2a6c23d4b830fcf9
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-03-26 13:54:53 +0530

    Add files via upload
    Today, I created a program to calculate the area of a circle, marking another milestone in our journey of exploration and learning.

---
+++ P_Qs2.c
@@ -0,0 +1,10 @@
+//Write a program to calculate area of a circle.
+#include<stdio.h>
+int main()
+{
+    float radius;
+    printf("\nEnter radius of a circle:");
+    scanf("%f",&radius);
+    printf("\nArea: %f",3.14*radius*radius);
+    return 0;
+}
