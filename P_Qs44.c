22bd9fc4f82aec6007591443c35f45d7106b1e52
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-05-07 20:35:04 +0530

    Add files via upload
    Today's task: writing a program to check if a specific character is present in a given string. This exercise focuses on string traversal and character matching in C programming.

---
+++ P_Qs44.c
@@ -0,0 +1,25 @@
+//Check if a given character is present in a string or not.
+#include<stdio.h>
+
+void checkChar(char str[],char ch);
+
+int main()
+{
+    char str[]="Thomas";
+    char ch='o';
+    checkChar(str,ch);
+    return 0;
+}
+
+void checkChar(char str[],char ch)
+{
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==ch)
+        {
+            printf("Character is present");
+            return;
+        }
+    }
+    printf("Character is NOT present");
+}
