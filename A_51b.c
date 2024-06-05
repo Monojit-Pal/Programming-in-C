402c784e513d22485ee690f73e81ea5f24f83e24
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-06-05 18:56:33 +0530

    Add files via upload
    Today's challenge: writing a program to generate the following pattern.

---
+++ A_51b.c
@@ -0,0 +1,23 @@
+/* Write program to generate the following pattern 
+            *
+           *  *
+        *   *    *
+    *     *   *    *   */
+
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,k;
+    printf("\nEnter number of lines: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(k=i;k<n;k++)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
