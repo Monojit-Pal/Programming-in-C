2088b9c23c2ac13b2be096c8e6f28e6eed23c370
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-07-03 10:59:30 +0530

    Add files via upload

---
+++ A_85.c
@@ -0,0 +1,24 @@
+//Write a program containing aa function that checks whether a given year is a leap year and returns 1 or accordingly.
+#include<stdio.h>
+
+int isleapyear(int year);
+
+int main()
+{
+    int y;
+    printf("\nEnter year: ");
+    scanf("%d",&y);
+    if(isleapyear(y))
+        printf("%d is leap year",y);
+    else
+        printf("%d is not leap year",y);
+    return 0;
+}
+
+int isleapyear(int year)
+{
+    if(year%4 == 0)
+        return 1;
+    else
+        return 0;
+}
